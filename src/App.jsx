6b364d8598210f3ac155074476854e9258e6eb27
abdullahshahid <abdullahshahid5112@gmail.com>
2025-01-31 15:08:33 +0500

    files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    
+        <div className="flex flex-col h-screen relative bg-green-500 text-white p-4">
+          <h1 className="text-7xl font-bold">Tailwind CSS is working!</h1>
+          <p className="mt-2">If you see this, Tailwind is configured correctly.</p>
+        </div>
+      );
+    
+}
+
+export default App;
